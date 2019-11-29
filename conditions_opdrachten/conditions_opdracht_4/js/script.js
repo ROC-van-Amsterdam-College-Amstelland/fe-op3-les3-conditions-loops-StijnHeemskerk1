68f54e6507fdaa0b3bd978846b098a0d7817164e
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-11-29 10:32:33 +0100

    opdracht 4

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,4 +1,7 @@
 //javascript code
 
-var myNumber = 
+var myNumber1 = 10;
 
+function click1(){
+    document.getElementById('text1').innerHTML= ( "De variabele myNumber heeft de waarde: " + myNumber1);
+}
