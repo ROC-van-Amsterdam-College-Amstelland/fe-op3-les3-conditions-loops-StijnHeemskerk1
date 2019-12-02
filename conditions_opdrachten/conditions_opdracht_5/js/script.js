44612e74c3aef10e67d316ccb6ffc676e3a627a2
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-02 13:52:06 +0100

    opdracht5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,11 +1,25 @@
 //javascript code
 
 
-var myOperator = + 
+var myOperator = prompt('+ of -'); 
 
+// function janee() {
+//     console.log(myOperator);
+// }
 
 function janee() {
-    if (myOperator = +) {
-        document.getElementById('').innerHTML='';
+    var myNumber1 = parseInt(document.getElementById('input1').value);
+    var myNumber2 = parseInt(document.getElementById('input2').value);
+ 
+    if (myOperator == '+') {
+        document.getElementById('div1').innerHTML = myNumber1 + myNumber2;
+        var myUitkomst = document.getElementById('div1').innerHTML;
+        document.getElementById('div1').innerHTML = "De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+
+    } else if (myOperator == '-') {
+        document.getElementById('div1').innerHTML = myNumber1 - myNumber2;
+        var myUitkomst = document.getElementById('div1').innerHTML;
+        document.getElementById('div1').innerHTML = "De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
     }
 } 
+
