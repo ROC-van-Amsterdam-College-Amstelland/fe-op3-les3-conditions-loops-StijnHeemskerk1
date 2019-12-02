61d873c59ea3e92bbb88b308547f832c30c09136
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-02 13:58:02 +0100

    opdracht6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+
+
+var myOperator = prompt('* of /'); 
+
+
+function janee() {
+    var myNumber1 = parseInt(document.getElementById('input1').value);
+    var myNumber2 = parseInt(document.getElementById('input2').value);
+ 
+    if (myOperator == '*') {
+        document.getElementById('div1').innerHTML = myNumber1 * myNumber2;
+        var myUitkomst = document.getElementById('div1').innerHTML;
+        document.getElementById('div1').innerHTML = "De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+
+    } else if (myOperator == '/') {
+        document.getElementById('div1').innerHTML = myNumber1 / myNumber2;
+        var myUitkomst = document.getElementById('div1').innerHTML;
+        document.getElementById('div1').innerHTML = "De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+    }
+} 
