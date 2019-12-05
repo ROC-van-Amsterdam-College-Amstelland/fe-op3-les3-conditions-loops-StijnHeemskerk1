8227ebc6ff8948e9c83f4fbdb16f1c0740f23139
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-05 15:27:19 +0100

    opdracht3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+
+for (i = 1; i <= 19; i++){
+    document.write(i, "<br>")
+}
+for (i = 20; i <= 40; i++){
+    document.write("Dit zijn mooie getallen", i, "<br>")
+}
+for (i = 41; i <= 50; i++){
+    document.write(i, "<br>")
+}
