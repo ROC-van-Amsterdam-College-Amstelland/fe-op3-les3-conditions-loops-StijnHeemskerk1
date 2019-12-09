f5eca366edc98a2f19f982b1d713a60b9ecc5ca1
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-09 14:22:53 +0100

    opdracht6

--- loops_opdrachten/loops_opdracht_6/js/script.js
+++ loops_opdrachten/loops_opdracht_6/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+var i;
+
+function button1(){
+    var myOutput = "";
+var maxNum = document.getElementById('input1').value;
+for (i = 1; i <= maxNum; i++) {
+    document.getElementById('output1').innerHTML=myOutput;
+    myOutput+=i + '<br>';
+    console.log(i);
+
+}
+}
