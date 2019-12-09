ddd96e3d6498f027c48954c2caf7e4971d77da97
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-09 13:19:14 +0100

    opdracht4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+var i;
+
+function button1(){
+    var myOutput = "";
+    for (i = 0; i <= 1000; i = i + 10){
+        console.log(i);
+        myOutput+=i + "<br>";
+        document.getElementById('output1').innerHTML=myOutput;
+        }
+    }
+
