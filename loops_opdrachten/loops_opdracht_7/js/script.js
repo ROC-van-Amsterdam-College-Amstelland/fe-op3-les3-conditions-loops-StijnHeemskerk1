8c47d615f589ee8ff43700eebdea4937e26cdc66
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-10 11:58:53 +0100

    opdracht7

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+
+var i;
+function button1(){
+    for (i = 0; i <= 100; i++) {
+        var vetNum = i%3;
+        var coolNum = i%5;
+        if (coolNum == 0) {
+            var coolGet = i / 5;
+            document.getElementById("output1").innerHTML += "<br> COOL " + coolGet * 5;
+        } else if (vetNum == 0){
+            var vetGet = i / 3;
+            document.getElementById('output1').innerHTML += "<br> VET " + vetGet * 3;
+        } else {
+            document.getElementById('output1').innerHTML += "<br>" + i;
+        }
+    }
+    }
