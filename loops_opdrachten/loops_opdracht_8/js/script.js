52aa79e52984c8b14485b33f3d056b2ded14a9f9
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-10 12:23:58 +0100

    opdracht8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,26 @@
 //javascript code
+
+var i;
+
+
+function button1(){
+    var getal1 ="";
+    for (i = 0; i <= 51; i++){
+        document.getElementById('output1').innerHTML=getal1;
+        getal1+="<br>" + i;
+        if (i == 25){
+            window.alert("Het getal 25 is net geen bullseye!");
+        } 
+    }
+}
+
+
+// function button1(){
+//     var getal1 ="";
+//     setTimeout(function () {
+//         for (i = 0; i <= 50; i++) {
+//             document.getElementById('output1').innerHTML=getal1;
+//             getal1+="<br>" + i;
+//             }   
+//         }, 3000); 
+// }
