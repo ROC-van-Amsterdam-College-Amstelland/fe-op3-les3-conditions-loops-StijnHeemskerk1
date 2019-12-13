f380a6b0816b7f0a014ecf297a1b5f415ae2ef3d
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-13 10:56:08 +0100

    opdracht8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -2,17 +2,36 @@
 
 var i;
 
-function button1() {
+// function button1() {
+//     var myOutput = "";
+//     for (i = 1; i <= 26; i++){
+//         document.getElementById('output1').innerHTML=myOutput;
+//         myOutput+="<br>" + i;
+//     }
+//     setTimeout(function(){ window.alert('25');}, 500);
+//     setTimeout(function(){ 
+//         for (i = 27; i <= 51; i++) {
+//             document.getElementById('output1').innerHTML=myOutput;
+//             myOutput+="<br>" + i;
+//         }
+//     }, 500)
+// }
+
+
+
+function button1(){
     var myOutput = "";
     for (i = 1; i <= 26; i++){
         document.getElementById('output1').innerHTML=myOutput;
         myOutput+="<br>" + i;
     }
-    setTimeout(function(){ window.alert('25');}, 500);
-    setTimeout(function(){ 
-        for (i = 27; i <= 51; i++) {
-            document.getElementById('output1').innerHTML=myOutput;
-            myOutput+="<br>" + i;
+    setTimeout(function(){
+        if (i == 27){
+            window.alert('Het getal 25 is net geen bullseye!');
+            for (i = 27; i <= 51; i++) {
+                document.getElementById('output1').innerHTML=myOutput;
+                myOutput+="<br>" + i;
+            }
         }
-    }, 500)
+    }, 1000)
 }
