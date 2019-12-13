7fee18ef433e17f620bc780095aa94f2a755e7e8
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2019-12-13 11:20:59 +0100

    opdracht9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,17 @@
 //javascript code
+
+var i;
+function button1(){
+    setTimeout(function(){for (i = 1; i <= 5; i++){
+        console.log(i);
+    }}, 500)
+    setTimeout(function(){if (i == 6){
+        document.getElementById('output1').innerHTML="Het einde is bereikt";
+    }}, 1000)
+    setTimeout(function(){for (i = 5; i >= 1; i = i - 1){
+        console.log(i)
+    }}, 2000)
+    setTimeout(function(){if (i == 0) {
+        document.getElementById('output2').innerHTML="Het begin is het begin";
+    }}, 2500)
+}
